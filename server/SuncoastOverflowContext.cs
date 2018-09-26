d8f433776ce8eeccf3b3526d397d87e25e0f90e8
Chris <chrispackett1998@gmail.com>
2018-09-26 15:40:00 -0400

    added entity framework deps, created db, created dbcontext

---
+++ server/SuncoastOverflowContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace server
+{
+    public partial class SuncoastOverflowContext : DbContext
+    {
+        public SuncoastOverflowContext()
+        {
+        }
+
+        public SuncoastOverflowContext(DbContextOptions<SuncoastOverflowContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("server=localhost;database=SuncoastOverflow");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
