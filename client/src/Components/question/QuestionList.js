c53f4ffcd27cacb158169c5282ea6500826c6833
Chris <chrispackett1998@gmail.com>
2018-09-29 16:03:40 -0400

    added search controller and fetching with search term, need to rethink

--- client/src/Components/question/QuestionList.js
+++ client/src/Components/question/QuestionList.js
@@ -6,11 +6,26 @@ class QuestionList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            questions: []
+            questions: [],
+            searchTerm: "",
+            header: "All Questions"
         }
     }
 
     componentDidMount() {
+        this.fetchQuestions()
+    }
+    
+    
+    updateSearchTerm = (userInput) => {
+        this.setState({
+            searchTerm: userInput
+        }, () => {
+            this.fetchSearchedQuestions()
+        })
+    } 
+    
+    fetchQuestions = () => {
         fetch("https://localhost:5001/api/posts/questions")
         .then(resp => resp.json())
         .then(questionData => {
@@ -19,12 +34,23 @@ class QuestionList extends Component {
             })
         })
     }
-    
+
+    fetchSearchedQuestions = () => {
+        fetch(`https://localhost:5001/api/search?q=${this.state.userInput}`)
+        .then(resp => resp.json())
+        .then(searchedQuestionData => {
+            this.setState({
+                questions: searchedQuestionData.results,
+                header: "Questions that related to " + this.state.userInput
+            })
+        })  
+    }
+
     render() {
         return (
             <div>
                 <div className="d-flex justify-content-around mb-4">
-                    <h2>All Questions</h2>
+                    <h2>{this.state.header}</h2>
                     <Link to="/question/add" className="btn btn-primary btn-lg align-self-center">Ask a Question</Link>
                 </div>
                 <div className="row">
--- server/Controllers/SearchController.cs
+++ server/Controllers/SearchController.cs
@@ -16,5 +16,26 @@ namespace server.Controllers
         {
             this.db = _db;
         }
+
+        public class ResponseObject
+        {
+            public bool WasSuccessful { get; set; }
+            public object Results { get; set; }
+        }
+
+        //GET api/search?q={search_term}
+        [HttpGet]
+        public ActionResult<ResponseObject> Get([FromQuery] string q)
+        {
+            var _searchedQuestions = this.db.Questions.Where(w => w.Title.Contains(q));
+
+            var _rv = new ResponseObject
+            {
+                WasSuccessful = true,
+                Results = _searchedQuestions
+            };
+
+            return _rv;
+        }
     }
 }
