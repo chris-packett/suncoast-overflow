37e75b9e068ac2fec9b88d66e7f0fbb4937808ea
Alexis Grisham <alexiskjg16@gmail.com>
2018-10-01 15:51:13 -0400

    finaltouches

--- client/src/Components/layout/Navbar.js
+++ client/src/Components/layout/Navbar.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import logo from '../../assets/Stack-Overflow-Logo.png'
 import navButtons from '../../assets/Navpics.png'
+import history from '../../history'
 /*import searchError from '../question/searchError' */
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: ''
+        }
+    }
+    
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        history.replace("/searchError")
+        this.setState({
+            searchTerm: ''
+        })
+    }
+
     render() {
         return (
             <div className="sticky-top">
@@ -13,9 +36,12 @@ class Navbar extends Component {
                     <Link to="/" className="nav-brand">
                         <img src={logo} alt="suncoast-overflow" width="200" />
                     </Link>
-                    <form className="form-inline">
-                    <Link to="/searchError"><input className="form-control mr-sm-2" type="text" placeholder="Search...">
-                       </input></Link>
+                    <form className="form-inline" onSubmit={this.handleSubmit}>
+                        <input className="form-control mr-sm-2" type="text" placeholder="Search..."
+                            name="searchTerm" 
+                            onChange={this.handleChange}
+                        >
+                        </input>
                     </form>
                     <img src={navButtons} alt="nav-buttons" height="40" />
                 </nav>
